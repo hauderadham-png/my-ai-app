5c46af849d555eab60e8647c4d5cf85d74886495
hauderadham-png <hauderadham@gmail.com>
2026-04-23 09:47:50 +0100

    Update ai_app.py

--- ai_app.py
+++ ai_app.py
@@ -2,33 +2,28 @@ import streamlit as st
 import requests
 import io
 from PIL import Image
-
-# إعدادات الصفحة
 st.set_page_config(page_title="Tunisian AI Generator", page_icon="🤖")
 
 st.title("🤖 🇹🇳")
 st.markdown("---")
-
-# خانة إدخال الكلام
 st.subheader("أوصفلي التصويرة اللي تحب تصنعها:")
 prompt = st.text_input("أكتب بالإنجليزية ", "")
 
-# زر الصناعة
+
 if st.button("أصنع السحر ✨"):
     if prompt:
         with st.spinner('قاعد نخدم على تصويرتك...'):
-            # الرابط السريع (Pollinations AI) - ما يسخنش وما يطلبش وقت
             image_url = f"https://image.pollinations.ai/prompt/{prompt.replace(' ', '%20')}?width=1024&height=1024&model=flux"
             
             try:
                 response = requests.get(image_url)
                 image = Image.open(io.BytesIO(response.content))
                 
-                # إظهار النتيجة
+                
                 st.image(image, caption=f"النتيجة لـ: {prompt}", use_container_width=True)
                 st.success("مبروك! هاذي تصويرتك حضرت.")
                 
-                # زر التحميل
+                
                 buf = io.BytesIO()
                 image.save(buf, format="PNG")
                 st.download_button("تحميل الصورة 📥", buf.getvalue(), "ai_image.png", "image/png")
@@ -39,4 +34,4 @@ if st.button("أصنع السحر ✨"):
         st.warning("لازم تكتب حاجة باش الماكينة تخدم!")
 
 st.markdown("---")
-st.caption("برمجة وتطوير: [إسمك هنا] 🚀")
+st.caption("برمجة وتطوير: [Hayder Adham et Arij Benltaif] 🚀")
